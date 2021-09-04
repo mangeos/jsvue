3f7044fb567f48708f225c92716c5dae085b60f1
Magnus <mange.ostling@gmail.com>
2021-09-04 09:57:59 +0100

    kmom01

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <editor id='myeditor'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import editor from './components/editor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    editor
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    background-color: rgba(53, 108, 144, 0.7);
 }
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 60px;
+}
+
+#myeditor {
+    background-color: white;
+}
+.ck-editor__editable_inline {
+  min-height: 500px;
+}
+.toolbar {
+    display: flex;
+    flex-direction: row;
+}
+
+.button {
+    display: flex;
+    background-color: #4CAF50; /* Green */
+    color: white;
+    padding: 15px 32px;
+    text-align: left;
+    font-size: 16px;
+
+}
+
+.button:hover {
+  background-color: #3e8e41;
+}
+
 </style>
---
+++ src/components/editor.vue
@@ -0,0 +1,33 @@
+<template>
+      <div id="app">
+          <div class="toolbar">
+              <button class='button' v-on:click="save()" >save</button>
+          </div>
+          <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" ></ckeditor>
+  </div>
+</template>
+
+<script>
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
+export default {
+       name: 'editor',
+       data() {
+           return {
+               editor: ClassicEditor,
+               editorData: '<p>Content of the editor.</p>',
+               editorConfig: {
+                   // The configuration of the editor.
+               }
+           };
+       },
+       methods: {
+           save: function() {
+               console.log(this.editorData);
+           }
+       }
+   }
+
+</script>
